a05157ad074fe1854025dfef162d946e9ddfa6c5
prerna-barde26git config --global user.name <prernabarde98@gmail.com>
2024-12-16 12:21:32 +0530

    saved all files

---
+++ home.js
@@ -0,0 +1,22 @@
+ "use strict";
+
+const panels = document.querySelectorAll('.panel');
+
+
+panels.forEach((panels)=>{
+    panels.addEventListener('click',()=>{
+        removeActiveClass();
+        panels.classList.add('active');
+    })
+})
+
+function removeActiveClass(){
+    panels.forEach((panel)=>{
+        panel.classList.remove("active");
+    })
+}
+
+
+
+
+
